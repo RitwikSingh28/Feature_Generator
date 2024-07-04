2932f7a200c433de4ac3cab998d11e2db636c5f3
RitwikSingh28 <itsritwik26@gmail.com>
2024-07-04 20:29:08 +0530

    refactor create_dirs code

--- src/create_dirs.rs
+++ src/create_dirs.rs
@@ -1,58 +1,36 @@
 use std::path::PathBuf;
 
+fn create_sub_dirs(base: &mut PathBuf, subdirs: &[&str]) -> Result<(), std::io::Error> {
+    for subdir in subdirs {
+        base.push(subdir);
+        std::fs::create_dir_all(&base)?;
+        base.pop();
+    }
+
+    Ok(())
+}
+
 pub fn create_data_dir(base: &mut PathBuf) -> Result<(), std::io::Error> {
     base.push("data");
 
-    base.push("datasource");
-    std::fs::create_dir_all(&base)?;
+    let result = create_sub_dirs(base, &["datasource", "models", "repositories"]);
     base.pop();
-
-    base.push("models");
-    std::fs::create_dir_all(&base)?;
-    base.pop();
-
-    base.push("repositories");
-    std::fs::create_dir_all(&base)?;
-    base.pop();
-
-    base.pop();
-    Ok(())
+    result
 }
 
 pub fn create_domain_dir(base: &mut PathBuf) -> Result<(), std::io::Error> {
     base.push("domain");
 
-    base.push("entities");
-    std::fs::create_dir_all(&base)?;
+    let result = create_sub_dirs(base, &["entities", "repositories", "usecases"]);
     base.pop();
-
-    base.push("repositories");
-    std::fs::create_dir_all(&base)?;
-    base.pop();
-
-    base.push("usecases");
-    std::fs::create_dir_all(&base)?;
-    base.pop();
-
-    base.pop();
-    Ok(())
+    result
 }
 
 pub fn create_presentation_dir(base: &mut PathBuf) -> Result<(), std::io::Error> {
     base.push("presentation");
 
-    base.push("bloc");
-    std::fs::create_dir_all(&base)?;
+    let result = create_sub_dirs(base, &["bloc", "pages", "widgets"]);
     base.pop();
 
-    base.push("pages");
-    std::fs::create_dir_all(&base)?;
-    base.pop();
-
-    base.push("widgets");
-    std::fs::create_dir_all(&base)?;
-    base.pop();
-
-    base.pop();
-    Ok(())
+    result
 }
