03e1113a3109f9bb3ca5725f0284cd72ce8d567f
RitwikSingh28 <itsritwik26@gmail.com>
2024-07-06 12:12:27 +0530

    add cli parsing and better doc

---
+++ README.md
@@ -0,0 +1,15 @@
+# FeatureFolderizer
+
+FeatureFolderizer is a tool designed to help you set up the folder structure for new features in your Flutter projects following Clean Architecture principles. It automates the creation of directories and files, ensuring a consistent and organized project layout.
+
+## Features
+
+- **Automate Folder Creation**: Quickly create a standardized folder structure for new features.
+- **Clean Architecture**: Ensures that your feature directories adhere to Clean Architecture principles.
+- **Cross-Platform**: Available for Windows, macOS, and Linux.
+
+## Folder Structure
+
+The folder structure created by FeatureFolderizer is as follows:
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,51 @@
+use std::path::PathBuf;
+
+use clap::Arg;
 use create_dirs::create_directories;
 
 mod create_dirs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String>= std::env::args().collect();
+    let matches = clap::command!()
+        .author("Ritwik Singh itsritwik26@gmail.com")
+        .name("FeatureFolderizer")
+        .arg(
+            Arg::new("feature_name")
+            .short('f')
+            .long("feature")
+            .value_name("FEATURE_NAME")
+            .required(true)
+            .help("The name of the feature to be created")
+        )
+        .arg(
+            Arg::new("base_dir")
+            .short('b')
+            .long("base")
+            .value_name("BASE_DIR")
+            .help("The base directory of the Flutter project (default: current directory)")
+        )
+        .get_matches();
 
-    let feature_name = &args[1];
+    let feature_name = matches.get_one::<String>("feature_name").unwrap();
+    
+    let mut curr_dir = if let Some(base_dir) = matches.get_one::<String>("base_dir") {
+        PathBuf::from(base_dir)
+    } else {
+        std::env::current_dir()?
+    };
 
-    let mut curr_dir = std::env::current_dir()?;
     curr_dir.push("lib");
 
     let path_exists = curr_dir.try_exists()?;
     if !path_exists {
-        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::NotFound, "Current directory is not root of the Flutter Project")));
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::NotFound, "Current directory is not root of the Flutter Project")
+        ));
     }
 
+    println!("Creating directories for feature: {}", feature_name);
     let _ = create_directories(&mut curr_dir, &feature_name)?;
+    println!("Directory structure created successfully");
 
     Ok(())
 }
